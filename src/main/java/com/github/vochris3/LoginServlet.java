a3363611c069459eac0f9fd022bb7ecd90da99b1
Chris Vo <chris@yikesinc.com>
2020-12-30 00:00:08 -0500

    initial commit

---
+++ src/main/java/com/github/vochris3/LoginServlet.java
@@ -0,0 +1,28 @@
+package com.github.vochris3;
+
+/**
+ * -class-
+ * creator: Chris Vo
+ * date: 29 Dec 2020
+ * project: practice-site
+ * package: com.github.vochris3
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/login.do")
+public class LoginServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<head>");
+    }
+
+}
