53dc71e2c2bd5e11fccd513f0592f9e2464c4dc4
Chris Vo <chris@yikesinc.com>
2021-01-04 09:10:44 -0500

    01042021-add index jsp

--- src/main/java/com/github/vochris3/LoginServlet.java
+++ src/main/java/com/github/vochris3/LoginServlet.java
@@ -12,23 +12,31 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
 
 @WebServlet(urlPatterns = "/login.do")
 public class LoginServlet extends HttpServlet {
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    private String message;
+
+    public void init() {
+        message = "Hello World!";
+    }
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // print Hello
         PrintWriter out = response.getWriter();
-        out.println("<html>");
-        out.println("<head>");
+        out.println("<html><head>");
         out.println("<title>First Servlet WebApp</title>");
-        out.println("</head>");
-        out.println("<body>");
+        out.println("</head><body>");
         out.println("<h1>Chris First Servlet WebApp</h1>");
-        out.println("</body>");
-        out.println("</html>");
+        out.println("</body></html>");
+    }
+
+    public void destroy() {
+
     }
 
 }
